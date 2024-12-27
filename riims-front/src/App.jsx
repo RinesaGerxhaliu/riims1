5753bf9944e60b1a48f0e59a850b6af4f9442785
RinesaGerxhaliu <149331920+RinesaGerxhaliu@users.noreply.github.com>
2024-12-27 23:06:37 +0100

    Installed bootstrap and added login and register pages in front, also footer and navbar.
    Installed Bootstrap and tested it,  created functional login and register pages, and added a navbar and footer for improved navigation and layout.

--- riims-front/src/App.jsx
+++ riims-front/src/App.jsx
@@ -1,24 +1,448 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {jwtDecode} from "jwt-decode";
+import Login from './features/auth/login';
+import Register from './features/auth/register';
+import Footer from "./components/layout/footer";
+import LoggedInNavbar from "./components/layout/LoggedInNavbar";
+
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  // Check if the token is expired
+  const isTokenExpired = (token) => {
+    if (!token) return true;
+    try {
+      const decodedToken = jwtDecode(token);
+      const currentTime = Date.now() / 1000;
+      return decodedToken.exp < currentTime;
+    } catch (error) {
+      console.error("Error decoding token:", error);
+      return true;
+    }
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem("jwtToken");
+    if (token && !isTokenExpired(token)) {
+      try {
+        const decodedToken = jwtDecode(token);
+        setIsAdmin(decodedToken.role && decodedToken.role.toLowerCase() === "admin");
+        setIsLoggedIn(true);
+      } catch (error) {
+        console.error("Error decoding token:", error);
+        handleLogout();
+      }
+    } else {
+      handleLogout();
+    }
+  }, []);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+    localStorage.setItem("isLoggedIn", "true");
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setIsAdmin(false);
+    localStorage.removeItem("jwtToken");
+    localStorage.removeItem("isLoggedIn");
+  };
+
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Routes>
+        {isLoggedIn ? (
+          <>
+            {/* <Route
+              path="/"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container">
+                    <Home />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/edit-profile"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <EditProfile />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/eksperienca"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <Eksperienca />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/aftesite"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <Aftesite />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/specializimet"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <Specializimet />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/gjuhet"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <Gjuhet />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/licensat"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <Licensat />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/projekti"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <Projekti />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/punavullnetare"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <PunaVullnetare />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/edukimi"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <Edukimi />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/Home"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <Home />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/honorsandawards"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <HonorsAndAwards />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/mbikqyresitemave"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <MbikqyresITemave />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/publikimi"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <Publikimi />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/personDetails"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <PersonDetails />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/EditEksperienca/:id"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <EditEksperienca />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+
+            <Route
+              path="/EditLicensa/:id"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <EditLicensa />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/EditAftesia/:id"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <EditAftesia />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/EditGjuhet/:id"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <EditGjuhet />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/EditSpecializim/:id"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <EditSpecializim />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/EditProjekti/:id"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <EditProjekti />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/EditEdukimi/:id"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <EditEdukimi />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/EditPunaVullnetare/:id"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <EditPunaVullnetare />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/EditMbikqyres/:id"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <EditMbikqyres />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/EditPublikimi/:id"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <EditPublikimi />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/EditHonorsAndAwards/:id"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <EditHonorsAndAwards />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/manageniveliakademik"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <ManageNiveliAkademik />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route path="/thecv" element={
+              <>
+                <LoggedInNavbar handleLogout={handleLogout} />
+                <div className="container mt-4">
+                  <TheCV />
+                </div>
+                <Footer />
+              </>
+            } />
+            <Route
+              path="/dashboard"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <Dashboard />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/manage-languages"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <ManageLanguages />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+               <Route
+              path="/ManageInstitucioni"
+              element={
+                <>
+                  <LoggedInNavbar handleLogout={handleLogout} />
+                  <div className="container mt-4">
+                    <ManageInstituconet />
+                  </div>
+                  <Footer />
+                </>
+              }
+            /> */}
+            <Route path="*" element={<Navigate to="/" />} />
+          </>
+        ) : (
+          <>
+
+            <Route path="/login" element={<Login onLogin={handleLogin} />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </>
+        )}
+      </Routes>
+
     </div>
+  </Router>
   );
 }
 
